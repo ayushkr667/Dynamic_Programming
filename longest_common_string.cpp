2f7a0f39914bb97f430c11e87889e2f585530607
Ayush kumar <ayushkr667@gmail.com>
2021-05-05 22:59:58 +0530

    Update longest_common_string.cpp

--- longest_common_string.cpp
+++ longest_common_string.cpp
@@ -1,6 +1,5 @@
 /*Given two strings ‘X’ and ‘Y’, find the length of the longest common substring.
 Examples :
-
 Input : X = “GeeksforGeeks”, y = “GeeksQuiz”
 Output : 5
 The longest common substring is “Geeks” and is of length 5.
