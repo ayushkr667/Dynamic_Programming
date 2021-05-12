1a0bed6c0ccaf4ce707e61737c89098e0d193a88
Ayush kumar <ayushkr667@gmail.com>
2021-05-12 20:46:02 +0530

    Update minimum_coin_to_make_x.cpp

--- minimum_coin_to_make_x.cpp
+++ minimum_coin_to_make_x.cpp
@@ -4,6 +4,7 @@ and we have infinite supply of each of C = { C1, C2, .. , Cm}
 valued coins, what is the minimum number of coins to make the change?
 
 Examples:
+
 Input: coins[] = {25, 10, 5}, V = 30
 Output: Minimum 2 coins required
 We can use one coin of 25 cents and one of 5 cents 
